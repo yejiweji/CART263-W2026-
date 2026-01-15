1de2d7e36186753d0cded8fc761459a53332989d
Yejin Oh <yejinnn.oh@gmail.com>
2026-01-15 15:20:29 -0500

    q1 + organization

--- tasks/q1.js
+++ tasks/q1.js
@@ -1,43 +1,41 @@
 function setup() {
     createCanvas(600, 400);
-    background(255);
-
+    background(0o0);
+    
     let x, y, w, h;
     let r, g, b;
 
     /* Ellipse 1 */
-    x = 150;
-    y = 200;
-    w = 160;
+    x = 60;
+    y = 50;
+    w = 80;
     h = 80;
     r = 255;
-    g = 0;
-    b = 0;
-
+    g = 192;
+    b = 203;
     fill(r, g, b);
     ellipse(x, y, w, h);
 
     /* Ellipse 2 */
-    x = 300;
+    x = 130;
     y = 140;
     w = 100;
-    h = 160;
-    r = 0;
-    g = 255;
-    b = 0;
-
+    h = 100;
+    r = 255;
+    g = 105;
+    b = 180;
     fill(r, g, b);
     ellipse(x, y, w, h);
 
     /* Ellipse 3 */
-    x = 450;
-    y = 260;
-    w = 200;
-    h = 100;
-    r = 0;
+    x = 240;
+    y = 240;
+    w = 140;
+    h = 140;
+    r = 255;
     g = 0;
     b = 255;
-
     fill(r, g, b);
     ellipse(x, y, w, h);
 }
+
---
+++ tasks/q2.js
@@ -0,0 +1,41 @@
+// function setup() {
+//     createCanvas(600, 400);
+//     background(0o0);
+    
+//     let x, y, w, h;
+//     let r, g, b;
+
+//     /* Ellipse 1 */
+//     x = 60;
+//     y = 50;
+//     w = 80;
+//     h = 80;
+//     r = 255;
+//     g = 192;
+//     b = 203;
+//     fill(r, g, b);
+//     ellipse(x, y, w, h);
+
+//     /* Ellipse 2 */
+//     x = 130;
+//     y = 140;
+//     w = 100;
+//     h = 100;
+//     r = 255;
+//     g = 105;
+//     b = 180;
+//     fill(r, g, b);
+//     ellipse(x, y, w, h);
+
+//     /* Ellipse 3 */
+//     x = 240;
+//     y = 240;
+//     w = 140;
+//     h = 140;
+//     r = 255;
+//     g = 0;
+//     b = 255;
+//     fill(r, g, b);
+//     ellipse(x, y, w, h);
+// }
+
